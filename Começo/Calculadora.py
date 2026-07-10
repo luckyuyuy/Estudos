c9b9755c40b8e0499d70302c853f400f2d3f0a21
Lucas <lucasmarigo12@gmail.com>
2026-07-10 05:40:21 -0300

    :3

---
+++ Começo/Calculadora.py
@@ -0,0 +1,11 @@
+print("Seja bem vindo")
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+
+print(soma)
+print(f"O resultado da soma foi: {soma}")
---
+++ Começo/boas_vindas.py
@@ -0,0 +1,2 @@
+nome = input("Insira seu nome: ")
+print("Seja bem vindo, "+ nome)
